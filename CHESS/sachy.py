68a21ff02d53818e731ff557ff0fe6f4a2f63566
Jakary1 <kares-jakub@seznam.cz>
2024-02-22 23:04:12 +0100

    another change that makes the window work

--- CHESS/sachy.py
+++ CHESS/sachy.py
@@ -1,3 +1,14 @@
-import tkinter
+from tkinter.simpledialog import askinteger
+from tkinter import *
+from tkinter import messagebox
+top = Tk()
 
-tk = tkinter.Tk()
+top.geometry("100x100")
+def show():
+   num = askinteger("Input", "Input an Integer")
+   print(num)
+   
+B = Button(top, text ="Click", command = show)
+B.place(x=50,y=50)
+
+top.mainloop()
