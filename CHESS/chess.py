817f51641618ca367928fe374bfe5328d8af82ce
Jakary1 <kares-jakub@seznam.cz>
2024-02-22 20:16:14 +0100

    a little bit of change

--- CHESS/chess.py
+++ CHESS/chess.py
@@ -1,5 +1,6 @@
 import pygame,sys
-
+pygame.init
+#screen = pygame.display.set_mode((800,600))
 def main():
     pygame.init
     win = pygame.display.set_mode((900,500))
@@ -8,12 +9,12 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
-    win.fill(0,255,255)
+    win.fill(0,0,255)
     pygame.display.flip
     pygame.quit()
 
 if __name__ == '__main__':
-    main()            
+    main()        
 
 
 
